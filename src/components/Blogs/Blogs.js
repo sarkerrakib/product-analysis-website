dd6c80b253450ae07292b800cd16e105941b4479
sarkerrakib <freelancerrakib25@gmail.com>
2022-04-05 22:39:13 +0600

    Blogs page

--- src/components/Blogs/Blogs.js
+++ src/components/Blogs/Blogs.js
@@ -13,7 +13,7 @@ const Blogs = () => {
                     </h2>
                     <div id="panelsStayOpen-collapseOne" className="accordion-collapse collapse show" aria-labelledby="panelsStayOpen-headingOne">
                         <div className="accordion-body">
-                            <strong>This is the first item's accordion body.</strong> It is shown by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+                        Context is a built-in API introduced in React 16.3. It makes it conceivable to pass information from parent to kids settled where it counts the part tree straightforwardly, rather than going it down through a chain of props. It tends to be utilized to take care of similar issues as Redux does, however it doesn't give a severe method of state control to guarantee an unsurprising way of behaving. Notwithstanding its low-level nature, it very well may be the ideal arrangement now and again.
                         </div>
                     </div>
                 </div>
@@ -25,7 +25,9 @@ const Blogs = () => {
                     </h2>
                     <div id="panelsStayOpen-collapseTwo" className="accordion-collapse collapse" aria-labelledby="panelsStayOpen-headingTwo">
                         <div className="accordion-body">
-                            <strong>This is the second item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+                        Semantic is HTML code that utilizes HTML labels to portray the motivation behind page components successfully. Semantic HTML code conveys the importance of its components to the two PCs and people, which helps internet browsers, web search tools, assistive innovations, and human engineers comprehend the parts of a website page.
+
+                        The way to elegantly composed semantic HTML is the utilization of semantic labels. Semantic HTML labels have names that tell the individual or machine perusing the code how precisely they're intended to respond.
                         </div>
                     </div>
                 </div>
@@ -37,7 +39,9 @@ const Blogs = () => {
                     </h2>
                     <div id="panelsStayOpen-collapseThree" className="accordion-collapse collapse" aria-labelledby="panelsStayOpen-headingThree">
                         <div className="accordion-body">
-                            <strong>This is the third item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+                        <strong>Block Elements:</strong> The elements that start on another line are known as block elements. A block element takes up the full width of the substance. Not at all like inline, there exists a top and base edge for these components. Block-level components may just show up inside the body tag. Block-level components make a bigger construction than inline elements.
+                        <br/>
+                        <strong>Inline-Block Elements:</strong>The display value of inline-block works similarly to inline with one exception: the height and width of that element become modifiable.It has no tag examples as it can be applied to any tag using CSS.
                         </div>
                     </div>
                 </div>
